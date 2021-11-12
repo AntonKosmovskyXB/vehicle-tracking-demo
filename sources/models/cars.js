eac1b22f250d2daad0313373b68a3ea97dce01f8
tonykosmos <akosmovsky@xbsoftware.by>
2021-11-12 14:30:36 +0300

    init commit

---
+++ sources/models/cars.js
@@ -0,0 +1,15 @@
+const cars = new webix.DataCollection({
+	data: [
+		{id: 1, model: "Volvo", stateNumber: "C064MK78", tracker: "Глонасс", weight: 15, group: "Фургон", squad: "Одиночное транспортное средство", capacity: "От 1,5 до 16 тонн", photo: "../sources/assets/photo/volvo.jpg"},
+		{id: 2, model: "Scania", stateNumber: "C152MK34", tracker: "", weight: 18, group: "Рефрижератор", squad: "Автомобиль-прицеп", capacity: "Свыше 16 тонн", photo: "../sources/assets/photo/scania.jpg"},
+		{id: 3, model: "Man", stateNumber: "C478MK78", tracker: "GPS", weight: 15, group: "Контейнеровоз", squad: "Автомобиль-прицеп", capacity: "Свыше 16 тонн", photo: "../sources/assets/photo/man.jpg"},
+		{id: 4, model: "Volvo", stateNumber: "C772MK34", tracker: "GPS", weight: 14, group: "Автоцистерна", squad: "Автомобиль-прицеп", capacity: "Свыше 16 тонн", photo: "../sources/assets/photo/volvo.jpg"},
+		{id: 5, model: "Scania", stateNumber: "C481MK78", tracker: "Глонасс", weight: 20, group: "Бортовой", squad: "Одиночное транспортное средство", capacity: "Свыше 16 тонн", photo: "../sources/assets/photo/scania.jpg"},
+		{id: 6, model: "Man", stateNumber: "C912MK78", tracker: "Глонасс", weight: 15, group: "Фургон", squad: "Одиночное транспортное средство", capacity: "От 1,5 до 16 тонн", photo: "../sources/assets/photo/man.jpg"},
+		{id: 7, model: "Volvo", stateNumber: "C158MK34", tracker: "", weight: 15, group: "Фургон", squad: "Одиночное транспортное средство", capacity: "От 1,5 до 16 тонн", photo: "../sources/assets/photo/volvo.jpg"},
+		{id: 8, model: "Scania", stateNumber: "C344MK34", tracker: "Глонасс", weight: 18, group: "Автоцистерна", squad: "Автомобиль-прицеп", capacity: "Свыше 16 тонн", photo: "../sources/assets/photo/scania.jpg"},
+		{id: 9, model: "Man", stateNumber: "C078MK78", tracker: "GPS", weight: 15, group: "Автоцистерна", squad: "Автомобиль-прицеп", capacity: "Свыше 16 тонн", photo: "../sources/assets/photo/man.jpg"}
+	]
+});
+
+export default cars;
---
+++ sources/models/trackers.js
@@ -0,0 +1,14 @@
+const trackers = new webix.DataCollection({
+	data: [
+		{id: 1, type: "Глонасс", model: "GloTrack 17VX", serialNumber: "SP109G481LJ2", brand: "Volvo", stateNumber: "C064MK78"},
+		{id: 2, type: "Глонасс", model: "GloTrack 2N", serialNumber: "SP109G481KP3", brand: "Man", stateNumber: "C152MK34"},
+		{id: 3, type: "GPS", model: "G-Mark Pointer 1", serialNumber: "SP109G481KP3", brand: "Scania", stateNumber: "C478MK78"},
+		{id: 4, type: "GPS", model: "G-Mark Pointer 2N", serialNumber: "SP109G481KP3", brand: "Volvo", stateNumber: "C772MK34"},
+		{id: 5, type: "Глонасс", model: "GloTrack 17VX", serialNumber: "SP109G481KP3", brand: "Man", stateNumber: "C178MK78"},
+		{id: 6, type: "Глонасс", model: "GloTrack 2N", serialNumber: "SP109G481KP3", brand: "Scania", stateNumber: "C138MK78"},
+		{id: 7, type: "Глонасс", model: "GloTrack 2N", serialNumber: "SP109G481KP3", brand: "Volvo", stateNumber: "C152MK78"},
+		{id: 8, type: "GPS", model: "G-Mark Pointer 1", serialNumber: "SP109G481KP3", brand: "Scania", stateNumber: "C912MK34"}
+	]
+});
+
+export default trackers;
