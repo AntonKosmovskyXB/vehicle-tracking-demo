360eea9958ac2d431da9aa83f62db262d5a7852d
tonykosmos <akosmovsky@xbsoftware.by>
2021-11-22 15:13:08 +0300

    fix bugs and styles in cars page

--- sources/views/top.js
+++ sources/views/top.js
@@ -6,27 +6,24 @@ export default class TopView extends JetView {
 			view: "menu",
 			id: "top:menu",
 			css: "app_menu",
-			width: 100,
+			width: 56,
 			layout: "y",
 			select: true,
 			template: "<span class='webix_icon #icon#'></span> #value# ",
 			data: [
-				{value: "", id: "map", icon: "mdi mdi-flag"},
+				{value: "", id: "map", icon: "mdi mdi-map"},
 				{value: "", id: "trackers", icon: "mdi mdi-crosshairs-gps"},
 				{value: "", id: "cars", icon: "mdi mdi-truck"},
 				{value: "", id: "drivers", icon: "mdi mdi-account"},
-				{value: "", id: "dashboard", icon: "mdi mdi-chart-box"}
+				{value: "", id: "dashboard", icon: "mdi mdi-view-dashboard"}
 			]
 		};
 
 		const ui = {
 			type: "clean",
-			paddingX: 5,
 			css: "app_layout",
 			cols: [
 				{
-					paddingX: 5,
-					paddingY: 10,
 					rows: [menu]
 				},
 				{
@@ -34,7 +31,7 @@ export default class TopView extends JetView {
 						{
 							view: "toolbar",
 							borderless: true,
-							height: 60,
+							height: 57,
 							cols: [
 								{
 									view: "label",
@@ -45,8 +42,6 @@ export default class TopView extends JetView {
 						},
 						{
 							type: "wide",
-							paddingY: 10,
-							paddingX: 5,
 							rows: [
 								{$subview: true}
 							]
@@ -61,5 +56,6 @@ export default class TopView extends JetView {
 
 	init() {
 		this.use(plugins.Menu, "top:menu");
+		webix.CustomScroll.init();
 	}
 }
